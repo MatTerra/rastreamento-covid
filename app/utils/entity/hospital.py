ebfd1121ef063b499e2bfd2a541da405c1e51cc3
titosilva <jt.mat@hotmail.com>
2020-11-21 15:14:28 -0300

    feature(entity): adding hospital entity

---
+++ app/utils/entity/hospital.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass, field
+
+from nova_api.entity import generate_id
+
+from utils.entity.base import Base
+
+@dataclass
+class Hospital(Base):
+    id_: str = field(default_factory=generate_id,
+                     metadata={"primary_key": True})
+    nome: str = field(default="")
+    local: str = field(default="")
