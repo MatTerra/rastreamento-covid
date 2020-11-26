6e2f4fdb14fa6f7befa88e4a14e9541cfc330523
titosilva <jt.mat@hotmail.com>
2020-11-25 23:21:25 -0300

    feat(entity): adding emissor entity

--- app/utils/entity/emissor.py
+++ app/utils/entity/emissor.py
@@ -5,7 +5,7 @@ from nova_api.entity import generate_id
 from utils.entity.base import Base
 
 @dataclass
-class Medicamento(Base):
+class Emissor(Base):
     id_: str = field(default_factory=generate_id,
                      metadata={"primary_key": True})
     nome: str = field(default="")
