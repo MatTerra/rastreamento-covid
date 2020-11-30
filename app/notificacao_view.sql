5e1869f426ddac742ee174891af2b5987ae2c661
MatTerra <mateus.b.s.terra@gmail.com>
2020-11-30 09:48:56 -0300

    Update notificacao_view

--- app/notificacao_view.sql
+++ app/notificacao_view.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW notificacao_nao_recebida AS
+CREATE OR REPLACE VIEW notificacao_view AS
     SELECT checkin_id_, checkin_id_usuario,
            checkin_risco, checkin_inicio, checkin_final
            local_id_, local_nome, notificacao_recebida
