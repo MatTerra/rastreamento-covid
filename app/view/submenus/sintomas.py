99e41a8c5b2e23789c5022777521ef9766f9631c
titosilva <jt.mat@hotmail.com>
2020-11-30 08:14:36 -0300

    feat(entity,dao): adding caso sintoma entity and DAO

--- app/view/submenus/sintomas.py
+++ app/view/submenus/sintomas.py
@@ -1,24 +1,25 @@
 from os import system
 from utils.entity.usuario import Usuario
-from utils.controller.checkin import view_checkins, create_checkin
+from utils.controller.sintoma import view_sintomas, create_sintoma
 from utils.colors import bcolors
 
-def checkin_menu(user: Usuario):
+def sintoma_menu(user: Usuario):
     while True:
         print(f"{bcolors.HEADER}{bcolors.BOLD}"
               f"O que você gostaria de fazer?"
               f"{bcolors.ENDC}\n")
-        print("\tl - Listar checkins")
-        print("\tc - Cadastrar um novo checkin")
+        print("\tl - Listar meus sintomas")
+        print("\tc - Cadastrar um novo sintoma que apresentei")
+        print("\ts - Listar os tipos de sintomas")
         print("\tx - Voltar ao menu principal")
         option = input(" >> ")
         if option == 'l':
             pagina = 0
             while pagina != 'x':
                 system("clear")
-                pagina = view_checkins(pagina, user)
+                pagina = view_sintomas(pagina, user)
         elif option == 'c':
-            create_checkin(user)
+            create_sintoma(user)
         elif option == 'x':
             system("clear")
             return user
